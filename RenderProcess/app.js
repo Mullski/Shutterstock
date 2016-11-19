aca2bdb3b6f9576af59f8ce0ec5a1535ebf61ee8
Sebastian Streich <streich.mobile@gmail.com>
2016-11-19 14:50:31 +0100

    finished the OverViewPage

--- RenderProcess/app.js
+++ RenderProcess/app.js
@@ -5,6 +5,7 @@
 //Ornder anlegen automatisch und sortiert
 //Funktion hinzufügen für automatischen download aller Ornder
 const {ipcRenderer} = require('electron')
+const { remote } =require('electron');
 const ShutterServiceAPI = require('electron').remote.require('./ShutterService.js');
 window.addEventListener('load',()=>{
 
