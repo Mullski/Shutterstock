3f3607401e6f51c413e5510fecdeebcb83b7687d
Sebastian Streich <streich.mobile@gmail.com>
2016-11-19 14:34:04 +0100

    Added Overview Dashboard

--- RenderProcess/elements/rush-overview/rush-overview.js
+++ RenderProcess/elements/rush-overview/rush-overview.js
@@ -8,21 +8,47 @@ Polymer({
     is: "rush-overview",
 
     created: function () {
-       
+
     },
 
     attached: function () {
-        
+
     },
-    setList:function(boxes){
-        this.set("lightboxes",boxes);
-        var sel=0;
-        for(var i=0; i < boxes.length; i++){
-            if(boxes[i].selected){
-                sel=sel+boxes[i].total_item_count;
+    setList: function (boxes) {
+        this.set("lightboxes", boxes);
+        var sel = 0;
+        for (var i = 0; i < boxes.length; i++) {
+            if (boxes[i].selected) {
+                sel = sel + boxes[i].total_item_count;
             }
         }
-        this.set("selectedImages",sel);
+        this.set("selectedImages", sel);
+
+        function checkIfValid(element) {
+            var expirationDate = element.expiration_time;
+            var localDate = new Date();
+            var timeStempLocal = Math.round(new Date(localDate).getTime() / 1000);
+            var timeStemp = Math.round(new Date(expirationDate).getTime() / 1000);
+            return timeStempLocal < timeStemp
+        }
+        //Fetch all Subscriptions Available
+        ShutterServiceAPI.fetchUserSubs((err, data) => {
+            if (err == null) {
+                var Subscriptions = data.data;
+                var runningSubscriptions = Subscriptions.filter(checkIfValid)
+                if(runningSubscriptions.length>0){
+                    this.set("subscription",runningSubscriptions[0])
+                    var time = new Date(runningSubscriptions[0].expiration_time);
+                    this.set("timeout",time.toLocaleString());
+                }
+                
+
+            }
+
+        });
+
+
+
     }
 
 });
